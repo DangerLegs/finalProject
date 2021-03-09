0eab1ce95dff1e0906977865b8ae3eccb7b26d96
CPMcshane <71343366+CPMcshane@users.noreply.github.com>
2021-03-10 04:27:19 +0800

    Add files via upload

---
+++ tiles_from_arcade.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tiles" tilewidth="32" tileheight="32" tilecount="2496" columns="52">
+ <image source="../../../../AppData/Roaming/Python/Python37/site-packages/arcade/resources/images/spritesheets/tiles.png" width="1664" height="1536"/>
+</tileset>
