eb5dbe97e5d1825efb6170767207cbb6e66167e9
DangerLegs <brytonpetersen@protonmail.com>
2021-03-29 22:14:30 -0600

    Changed the intro windows

--- introwindows.py
+++ introwindows.py
@@ -22,8 +22,12 @@ class InstructionView(arcade.View):
     def on_draw(self):
         """ Draw this view """
         arcade.start_render()
-        arcade.draw_text("Instructions Screen", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
-                         arcade.color.WHITE, font_size=50, anchor_x="center")
+        arcade.draw_text("Oh no! King Dino is on a rampage! The town is going to be reduced to rubble if he's not stopped! \n"
+                         "His water was poisened and now hes acting like a savage beast! There is only one special \n"
+                         "potion that can make it pure again. You are the only one who can help him. Do you have what it \n"
+                         "takes to overcome the sinister forces that stand in your way and stop the King? \n"
+                         "Proceed if you dare!", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
+                         arcade.color.WHITE, font_size=23, anchor_x="center")
         arcade.draw_text("Click to advance", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2-75,
                          arcade.color.WHITE, font_size=20, anchor_x="center")
 
@@ -44,8 +48,8 @@ class IntroView(arcade.View):
     def on_draw(self):
         """ Draw this view """
         arcade.start_render()
-        arcade.draw_text("Instructions Screen", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
-                         arcade.color.WHITE, font_size=50, anchor_x="center")
+        arcade.draw_text("Objective: Find the potion to clean King Dino's water supply.", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
+                         arcade.color.WHITE, font_size=36, anchor_x="center")
 
         arcade.draw_text("Click to advance", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2-75,
                          arcade.color.WHITE, font_size=20, anchor_x="center")
